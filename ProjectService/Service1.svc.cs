8f32a79294e0610acdd851b11dc248987b4a81fc
JPostigo48 <68659952+JPostigo48@users.noreply.github.com>
2023-08-01 11:43:39 -0500

    Add files via upload

---
+++ ProjectService/Service1.svc.cs
@@ -0,0 +1,28 @@
+﻿using ProjectDB.Dao;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProjectService
+{
+    public class Service1 : IService1
+    {
+        public string addAlumno(IList<String> data)
+        {
+            Alumno a = new Alumno();
+            string s = a.addAlumno(data);
+            return s;
+        }
+        public IList<String> getCiudades()
+        {
+            Ciudad c = new Ciudad();
+            IList<String> ciudades = c.getCiudades();
+            return ciudades;
+        }
+    }
+}
